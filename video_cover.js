1ce33c44389b9973fd46b304658289f78a41b283
Martin Gottweis <gottwik@gmail.com>
2016-11-03 18:44:02 +0100

    added demo

--- video_cover.js
+++ video_cover.js
@@ -4,61 +4,78 @@
 // *	stretches video to cover it's parent
 // * ———————————————————————————————————————————————————————— * //
 
-(function ($) {
+function cover (video_element) {
+	console.log(video_element)
 
-	$.fn.video_cover = function () {
+	// direct parent of the video tag
+	container = video_element.parent()
 
-		// direct parent of the video tag
-		container = this.parent()
+	// set overflow to hidden for the video container
+	container.css('overflow', 'hidden')
 
-		// video tag itself
-		video_element = this
+	// makes sure the resizing is not done more than 5 times per second
+	timeout_throttle = setTimeout(resize_video, 200)
 
-		// set overflow to hidden for the video container
-		container.css('overflow', 'hidden')
+	// main resizing function
+	function resize_video () {
 
-		// makes sure the resizing is not done more than 5 times per second
-		timeout_throttle = setTimeout(resize_video, 200)
+		// calculates aspect ratio of the video
+		var video_ratio = video_element.width() / video_element.height()
 
-		// main resizing function
-		function resize_video () {
+		// calculates ratio of the container
+		var size_ratio = container.width() / container.height()
 
-			// calculates aspect ratio of the video
-			var video_ratio = video_element.width() / video_element.height()
+		// checks if video is more oblong than the container
+		if (size_ratio < video_ratio) {
+			// if it's more oblong, that means the height of the video will be the same as the height of the container
+			// and the video will overflow to the sides
+			video_element
+				.css('height', container.height())
+				.css('width', container.height() * video_ratio)
+				.css('left', -(container.width() - video_element.height()) / 2)
+		} else {
+			// if the video is less oblong, width of the video and the container will match and the video will overflow to the bottom
+			video_element
+				.css('width', container.width())
+				.css('height', container.width() / video_ratio)
+				.css('left', 0)
+		}
+	}
 
-			// calculates ratio of the container
-			var size_ratio = container.width() / container.height()
+	// resizes the video on window resize event
+	$(window).on('resize', function () {
 
-			// checks if video is more oblong than the container
-			if (size_ratio < video_ratio) {
-				// if it's more oblong, that means the height of the video will be the same as the height of the container
-				// and the video will overflow to the sides
-				video_element
-					.css('height', container.height())
-					.css('width', container.height() * video_ratio)
-					.css('left', -(container.width() - video_element.height()) / 2)
-			} else {
-				// if the video is less oblong, width of the video and the container will match and the video will overflow to the bottom
-				video_element
-					.css('width', container.width())
-					.css('height', container.width() / video_ratio)
-					.css('left', 0)
-			}
+		// cancel the scheduled resizing if there is any
+		if (timeout_throttle) {
+			clearTimeout(timeout_throttle)
 		}
 
-		// resizes the video on window resize event
-		$(window).on('resize', function () {
+		// schedule the resizing in 200ms
+		timeout_throttle = setTimeout(resize_video, 100)
+	})
+}
 
-			// cancel the scheduled resizing if there is any
-			if (timeout_throttle) {
-				clearTimeout(timeout_throttle)
-			}
-
-			// schedule the resizing in 200ms
-			timeout_throttle = setTimeout(resize_video, 100)
-		})
+// extend jquery if jquery is initialized
+if (typeof $ !== 'undefined') {
+	$.fn.video_cover = function () {
+		cover(this)
 
 		// return itself for chaining
 		return this
 	}
-}(jQuery))
+}
+
+// commonJs module
+define(['jquery'],
+	function ($) {
+		var video_cover = function () {}
+
+		video_cover.cover = function (elements) {
+			elements.each(function (index, element) {
+				cover($(element))
+			})
+		}
+
+		return video_cover
+	}
+)
